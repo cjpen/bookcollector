30f89b91051fef64e941035209865c4b656e0f96
cjpen <cjpen@me.com>
2022-02-11 01:58:38 -0800

    Lab 2 & 3

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    pub_date = models.CharField(max_length=4)
+    isbn = models.CharField(max_length=13)
+
+    def __str__(self):
+        return self.title
+    
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'book_id': self.id})
--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from .models import Book
 
 # Create your views here.
 def home(request):
@@ -8,4 +10,22 @@ def about(request):
     return render(request, 'about.html')
 
 def books_index(request):
-    return render(request, 'books/index.html', { 'books': 'books'})
+    books = Book.objects.all()
+    return render(request, 'books/index.html', { 'books': books })
+
+def books_detail(request, book_id):
+    book = Book.objects.get(id=book_id)
+    return render(request, 'books/detail.html', { 'book': book })
+
+class BookCreate(CreateView):
+    model = Book
+    fields = '__all__'
+    success_url = '/books/'
+
+class BookUpdate(UpdateView):
+    model = Book
+    fields = '__all__'
+
+class BookDelete(DeleteView):
+    model = Book
+    success_url = '/books/'
